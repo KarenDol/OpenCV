c36dacc0e4afed79743154927b31c4b42d4fc5d4
KarenDol <lil_himick@kaist.ac.kr>
2024-09-30 13:22:04 +0900

    Alert system improved, the violation report feature was implemented

--- CV/views.py
+++ CV/views.py
@@ -3,11 +3,14 @@ import os
 from django.conf import settings
 from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
+from datetime import datetime
 
 def save_video(request):
     if request.method == 'POST' and request.FILES.get('video'):
         video_file = request.FILES['video']
-        
+        VideoReport = request.POST['VideoReport'] #Реропт о нарушениях
+        print(VideoReport)
+
         # Define the path to save the video (static/videos/)
         static_videos_path = os.path.join(settings.BASE_DIR, 'CV', 'static', 'videos')
         if not os.path.exists(static_videos_path):
@@ -15,8 +18,13 @@ def save_video(request):
 
         # Save the file using Django's FileSystemStorage
         fs = FileSystemStorage(location=static_videos_path)
-        filename = fs.save(video_file.name, video_file)
-        file_url = fs.url(filename)
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        filename = f'recorded_video_{timestamp}.webm'  # Use a meaningful name
+
+        fs = FileSystemStorage(location=static_videos_path)
+        saved_filename = fs.save(filename, video_file)
+
+        file_url = os.path.join('static', 'videos', saved_filename)
 
         return JsonResponse({'message': 'Video saved successfully!', 'video_url': file_url})
     return JsonResponse({'error': 'Invalid request'}, status=400)
